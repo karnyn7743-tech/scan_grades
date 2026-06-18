bbc432609144e9a7aad0bedcdfd983b351771bce
karnyn7743-tech <karnyn7743@gmail.com>
2026-06-18 21:05:31 +0300

    Delete android directory

--- android/app/src/main/MainActivity.kts
+++
@@ -1,6 +0,0 @@
-packge com.example.student_grades_scanner
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // لا تضع أي شيء هنا إلا إذا كنت تعرف سبب وجوده
-}
