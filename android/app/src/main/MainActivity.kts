13946340cf6d8ce2c86395179b6e8524308d08ac
karnyn7743-tech <karnyn7743@gmail.com>
2026-06-18 20:16:42 +0300

    Add files via upload

---
+++ android/app/src/main/MainActivity.kts
@@ -0,0 +1,6 @@
+packge com.example.student_grades_scanner
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // لا تضع أي شيء هنا إلا إذا كنت تعرف سبب وجوده
+}
