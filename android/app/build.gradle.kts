6a038b39bd19e71974b75a2e155fc48d2cbff699
karnyn7743-tech <karnyn7743@gmail.com>
2026-06-19 15:25:23 +0300

    Update Android app configuration and namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.control_students_grades"
+    namespace = "com.example.school_grading_app"
     compileSdk = 35
-    ndkVersion = flutter.ndkVersion // تم تعديل هذا السطر بإضافة علامات التنصيص لحل المشكلة
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -16,25 +14,22 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.control_students_grades"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        applicationId = "com.example.school_grading_app"
+        minSdk = 21
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
