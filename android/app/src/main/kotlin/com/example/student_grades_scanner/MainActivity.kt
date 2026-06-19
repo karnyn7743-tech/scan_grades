e3830b340d34461f87697e55c5466b9614c0f9b5
karnyn7743-tech <karnyn7743@gmail.com>
2026-06-19 12:12:45 +0300

    Delete android directory

--- android/app/src/main/kotlin/com/example/student_grades_scanner/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.student_grades_scanner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // هذا النشاط فارغ، كل شيء يُدار عبر Flutter
-}
