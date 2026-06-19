d8b8735a6db1a10833f2f631b6634339e63ff7a2
karnyn7743-tech <karnyn7743@gmail.com>
2026-06-19 11:10:14 +0300

    Correct package declaration and update comments
    Fix package declaration and improve comments in MainActivity.

--- android/app/src/main/kotlin/com/example/student_grades_scanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/student_grades_scanner/MainActivity.kt
@@ -1,6 +1,7 @@
-packge com.example.student_grades_scanner
+package com.example.student_grades_scanner
+
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // لا تضع أي شيء هنا إلا إذا كنت تعرف سبب وجوده
+    // هذا النشاط فارغ، كل شيء يُدار عبر Flutter
 }
