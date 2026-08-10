d1fc631ac817e0dbc54954f4a07d0b0126a1f7cb
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-08-10 22:09:03 +0300

    Update application ID and namespace in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.scanner_grades_withcode"
+    namespace = "com.example.wifi_p2p_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.correcting_papers_withcode"
+        applicationId = "com.example.wifi_p2p_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
